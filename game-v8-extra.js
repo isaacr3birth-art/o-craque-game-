28222568f6255d92be7570a65135ddc9e4de0203
isaacr3birth-art <isaac.r3birth@gmail.com>
2026-09-10 21:39:24 -0300

    fix: load international stat integrity patch

--- game-v8-extra.js
+++ game-v8-extra.js
@@ -1,5 +1,5 @@
 (()=>{
 'use strict';
-const scripts=['game-v8-extra-core.js','game-v8-integrity.js'];
+const scripts=['game-v8-extra-core.js','game-v8-integrity.js','game-v8-integrity-fixes.js'];
 for(const src of scripts){if(!document.querySelector(`script[src="${src}"]`)){const s=document.createElement('script');s.src=src;s.defer=true;document.head.appendChild(s)}}
 })();
